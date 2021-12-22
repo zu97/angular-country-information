e4d8ab5cf9a4fc0a9045c719475752260fecb558
admin <admin@admin.com>
2021-12-22 13:00:06 +0600

    Initial project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'homework66';
-}
+export class AppComponent {}
